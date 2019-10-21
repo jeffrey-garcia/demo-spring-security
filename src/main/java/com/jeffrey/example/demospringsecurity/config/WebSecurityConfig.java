1511487bf36ee3e325cedfd569169547ec21f21e
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2019-10-21 08:52:12 +0800

    ignore security check for un-secured resources

--- src/main/java/com/jeffrey/example/demospringsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/jeffrey/example/demospringsecurity/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -54,6 +55,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     @Qualifier("userDetailsService")
     UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // decouple unsecured resources from secured resources (which should be ignored by spring security)
+        web
+            .ignoring()
+                .antMatchers(
+                        "/resources/**",
+                        "/images/**",
+                        "/styles/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
